943735528e69dd96d835533c407ec006ac671183
Tanyaa-a <120506794+Tanyaa-a@users.noreply.github.com>
2024-04-12 17:07:07 -0700

    add lesson1_7

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -3,8 +3,6 @@ import InputWithLabel from "./InputWithLabel";
 
 const TodoForm = ({onAddTodo}) => {
   const [todoTitle, setTodoTitle] = React.useState("");
-  const handleChange = (event) => {
-  };
   const handleTitleChange = (event) => {
     let newTodoTitle = event.target.value;
     setTodoTitle(newTodoTitle);

1cf1a7aa085a929189e7b6ab69ca674348812fd1
Tanyaa-a <120506794+Tanyaa-a@users.noreply.github.com>
2024-04-30 17:43:16 -0700

    add lesson 1_8

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,51 @@
-import React, { useEffect } from "react";
+import { useState, useEffect } from "react";
 import TodoList from "./TodoList";
 import TodoForm from "./AddTodoForm";
 
-// Custom hook to manage semi-persistent state using localStorage
-const useSemiPersistentState = () => {
-  const [todoList, setTodoList] = React.useState(
-    JSON.parse(localStorage.getItem("savedTodoList")) || []
-  );
+function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  // Fetch data from Airtable
+  async function fetchData() {
+    const options = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`,
+      },
+    };
+    const url = `https://api.airtable.com/v0/${
+      import.meta.env.VITE_AIRTABLE_BASE_ID
+    }/${import.meta.env.VITE_TABLE_NAME}`;
+    try {
+      const response = await fetch(url, options);
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+
+      const data = await response.json();
+//accepts the results of mapping data.records
+      const todos = data.records.map((todo) => ({
+        id: todo.id,
+        title: todo.fields.title,
+      }));
+
+      setTodoList(todos);
+      setIsLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+// Fetch data from Airtable when component mounts
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   useEffect(() => {
-    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
-  }, [todoList]);
+    if (!isLoading)
+      localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+  }, [todoList, isLoading]);
 
-  return [todoList, setTodoList];
-};
-
-function App() {
-  const [todoList, setTodoList] = useSemiPersistentState();
   function addTodo(newTodo) {
     setTodoList([...todoList, newTodo]);
   }
@@ -29,7 +58,11 @@ function App() {
     <>
       <h1>Todo List</h1>
       <TodoForm onAddTodo={addTodo} />
-      <TodoList onRemoveTodo={removeTodo} todoList={todoList} />
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <TodoList onRemoveTodo={removeTodo} todoList={todoList} />
+      )}
     </>
   );
 }

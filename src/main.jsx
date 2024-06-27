b8f771a17e8fb574e3cd5bb1c53cf4cf9a72b3f4
Tanyaa-a <120506794+Tanyaa-a@users.noreply.github.com>
2024-06-27 12:03:09 -0700

    some changes

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,9 @@ import 'normalize.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App /> 
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
 
